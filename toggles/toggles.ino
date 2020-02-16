befdfb8783b481652d676a98f59881b547dccef4
Katherine Lasonde <kfml09@gmail.com>
2020-02-16 10:55:40 -0500

    updating the code for the dash

--- toggles/toggles.ino
+++ toggles/toggles.ino
@@ -1,26 +1,29 @@
 // TOGGLES
 
 //Constants for Toggle 1 (A&B)
-const int buttonPinToggle1A = 5;  
-const int buttonPinToggle1B = 6;    
-const int ledPinToggle1AB =  14;  
+const int buttonPinToggle1A = 14;  
+const int buttonPinToggle1B = 13;    
 
 //Constants for Toggle 2 (A&B)
-const int buttonPinToggle2A = 7;  
-const int buttonPinToggle2B = 8;    
-const int ledPinToggle2AB =  9; 
+const int buttonPinToggle2A = 12;  
+const int buttonPinToggle2B = 11;    
 
 //Variables for Toggles
-int stateToggle1 = HIGH;      // the current state of the output pin #1
-int readingToggle1;           // the current reading from the input pin #1
-int previousToggle1 = LOW;    // the previous reading from the input pin #1
+int stateToggle1A = HIGH;      // the current state of the output pin #1
+int readingToggle1A;           // the current reading from the input pin #1
+int previousToggle1A = LOW;    // the previous reading from the input pin #1
 
-int stateToggle2 = HIGH;      // the current state of the output pin #2
-int readingToggle2;           // the current reading from the input pin #2
-int previousToggle2 = LOW;    // the previous reading from the input pin #2
+int stateToggle1B = HIGH;      // the current state of the output pin #1
+int readingToggle1B;           // the current reading from the input pin #1
+int previousToggle1B = LOW;    // the previous reading from the input pin #1
 
-int flagToggle1 = 0; // 0, the LED is off; 1, LED is on
-int flagToggle2 = 0; // 0, the LED is off; 1, LED is on
+int stateToggle2A = HIGH;      // the current state of the output pin #2
+int readingToggle2A;           // the current reading from the input pin #2
+int previousToggle2A = LOW;    // the previous reading from the input pin #2
+
+int stateToggle2B = HIGH;      // the current state of the output pin #2
+int readingToggle2B;           // the current reading from the input pin #2
+int previousToggle2B = LOW;    // the previous reading from the input pin #2
 
 // the follow variables are long's because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
@@ -29,201 +32,136 @@ long debounce = 200;  // the debounce time, increase if the output flickers
 
 void setup()
 {
+  // both toggles are inputs
   pinMode(buttonPinToggle1A, INPUT);
-  pinMode(buttonPinToggle1B, OUTPUT);
-  pinMode(ledPinToggle1AB, OUTPUT);      
-
+  pinMode(buttonPinToggle1B, INPUT);
 
   pinMode(buttonPinToggle2A, INPUT);
-  pinMode(buttonPinToggle2B, OUTPUT);
-  pinMode(ledPinToggle2AB, OUTPUT);      
-
+  pinMode(buttonPinToggle2B, INPUT);
 }
 
 void loop()
 {
-  readingToggle1 = digitalRead(buttonPinToggle1A);
-  readingToggle2 = digitalRead(buttonPinToggle2A);
+  readingToggle1A = digitalRead(buttonPinToggle1A);
+  readingToggle1B = digitalRead(buttonPinToggle1B);
+
+  readingToggle2A = digitalRead(buttonPinToggle2A);
+  readingToggle2B = digitalRead(buttonPinToggle2B);
 
   // if the input just went from LOW and HIGH, toggle the output pin and remember the time
+
+  // TOGGLE 1
   
-  // Both toggles are HIGH
-  if ((readingToggle1 == HIGH && previousToggle1 == LOW) && (readingToggle2 == HIGH && previousToggle2 == LOW) && millis() - time > debounce) {
-    // switch toggle state 1 to high
-    if (stateToggle1 == HIGH) { stateToggle1 = LOW; }
-    else { stateToggle1 = HIGH; }
+  // Both toggles 1A and 1B are HIGH
+  if ((readingToggle1A == HIGH && previousToggle1A == LOW) && (readingToggle1B == HIGH && previousToggle1B == LOW) && millis() - time > debounce) {
+    // switch toggle state 1A to high
+    if (stateToggle1A == HIGH) { stateToggle1A = LOW; }
+    else { stateToggle1A = HIGH; }
 
-    // switch toggle state 2 to high
-    if (stateToggle2 == HIGH) { stateToggle2 = LOW; }
-    else { stateToggle2 = HIGH; }
-
-    // LEDS
-
-    // if the toggle 1 LED is on, turn it off 
-    if (flagToggle1 == 0) {
-      digitalWrite(ledPinToggle1AB, HIGH);
-      flagToggle1 = 1; //change flag variable; LED on
-    }
-    // if the LED is off, turn it on 
-    else if (flagToggle1 == 1){
-      digitalWrite(ledPinToggle1AB, HIGH);
-      flagToggle1 = 1; //change flag variable; LED on
-
-   // if the toggle 2 LED is on, turn it off 
-    if (flagToggle2 == 0) {
-      digitalWrite(ledPinToggle2AB, HIGH);
-      flagToggle2 = 1; //change flag variable; LED on
-    }
-    // if the LED is off, turn it on 
-    else if (flagToggle2 == 1){
-      digitalWrite(ledPinToggle2AB, HIGH);
-      flagToggle2 = 1; //change flag variable; LED on  
+    // switch toggle state 1B to high
+    if (stateToggle1B == HIGH) { stateToggle1B = LOW; }
+    else { stateToggle1B = HIGH; }
     
     time = millis();    
-  }
+    }
 
   // Toggle 1 is LOW (down) and toggle 2 is HIGH
-  else if ((readingToggle1 == LOW && previousToggle1 == HIGH) && (readingToggle2 == HIGH && previousToggle2 == LOW) && millis() - time > debounce) {
-    // switch toggle state 1 to low
-    if (stateToggle1 == LOW) { stateToggle1 = HIGH; }
-    else { stateToggle1 = LOW; }
+  else if ((readingToggle1A == LOW && previousToggle1A == HIGH) && (readingToggle1B == HIGH && previousToggle1B == LOW) && millis() - time > debounce) {
+    // switch toggle state 1A to low
+    if (stateToggle1A == LOW) { stateToggle1A = HIGH; }
+    else { stateToggle1A = LOW; }
 
-    // switch toggle state 2 to high
-    if (stateToggle2 == HIGH) { stateToggle2 = LOW; }
-    else { stateToggle2 = HIGH; }
-
-    // LEDS
-
-    // if the toggle 1 LED is on, turn it off 
-    if (flagToggle1 == 0) {
-      digitalWrite(ledPinToggle1AB, LOW);
-      flagToggle1 = 0; //change flag variable; LED off
-    }
-    // if the LED is off, turn it on 
-    else if (flagToggle1 == 1){
-      digitalWrite(ledPinToggle1AB, LOW);
-      flagToggle1 = 0; //change flag variable; LED off
-
-   // if the toggle 2 LED is on, turn it off 
-    if (flagToggle2 == 0) {
-      digitalWrite(ledPinToggle2AB, HIGH);
-      flagToggle2 = 1; //change flag variable; LED on
-    }
-    // if the LED is off, turn it on 
-    else if (flagToggle2 == 1){
-      digitalWrite(ledPinToggle2AB, HIGH);
-      flagToggle2 = 1; //change flag variable; LED on 
+    // switch toggle state 1B to high
+    if (stateToggle1B == HIGH) { stateToggle1B = LOW; }
+    else { stateToggle1B = HIGH; }
     
     time = millis();    
-  }
-
-  // Toggle 1 is LOW (down) and toggle 2 is LOW
-  else if ((readingToggle1 == LOW && previousToggle1 == HIGH) && (readingToggle2 == LOW && previousToggle2 == HIGH) && millis() - time > debounce) {
-    // switch toggle state 1 to low
-    if (stateToggle1 == LOW) { stateToggle1 = HIGH; }
-    else { stateToggle1 = LOW; }
-
-    // switch toggle state 2 to low
-    if (stateToggle2 == LOW) { stateToggle2 = HIGH; }
-    else { stateToggle2 = LOW; }
-
-    // LEDS
-
-    // if the toggle 1 LED is on, turn it off 
-    if (flagToggle1 == 0) {
-      digitalWrite(ledPinToggle1AB, LOW);
-      flagToggle1 = 0; //change flag variable; LED off
     }
-    // if the LED is off, turn it on 
-    else if (flagToggle1 == 1){
-      digitalWrite(ledPinToggle1AB, LOW);
-      flagToggle1 = 0; //change flag variable; LED off
-
-   // if the toggle 2 LED is on, turn it off 
-    if (flagToggle2 == 0) {
-      digitalWrite(ledPinToggle2AB, LOW);
-      flagToggle2 = 0; //change flag variable; LED off
-    }
-    // if the LED is off, turn it on 
-    else if (flagToggle2 == 1){
-      digitalWrite(ledPinToggle2AB, LOW);
-      flagToggle2 = 0; //change flag variable; LED off
-    
-    time = millis();    
-  }
 
   // Toggle 1 is HIGH and toggle 2 is LOW
-  else if ((readingToggle1 == HIGH && previousToggle1 == LOW) && (readingToggle2 == LOW && previousToggle2 == HIGH) && millis() - time > debounce) {
-    // switch toggle state 1 to high
-    if (stateToggle1 == HIGH) { stateToggle1 = LOW; }
-    else { stateToggle1 = HIGH; 
+  else if ((readingToggle1A == HIGH && previousToggle1A == LOW) && (readingToggle1B == LOW && previousToggle1B == HIGH) && millis() - time > debounce) {
+    // switch toggle state 1A to high
+    if (stateToggle1A == HIGH) { stateToggle1A = LOW; }
+    else { stateToggle1A = HIGH; }
 
-    // switch toggle state 2 to low
-    if (stateToggle2 == LOW) { stateToggle2 = HIGH; }
-    else { stateToggle2 = LOW; }
-
-    // LEDS
-
-    // if the toggle 1 LED is on, turn it off 
-    if (flagToggle1 == 0) {
-      digitalWrite(ledPinToggle1AB, HIGH);
-      flagToggle1 = 1; //change flag variable; LED on
-    }
-    // if the LED is off, turn it on 
-    else if (flagToggle1 == 1){
-      digitalWrite(ledPinToggle1AB, HIGH);
-      flagToggle1 = 1; //change flag variable; LED on
-
-   // if the toggle 2 LED is on, turn it off 
-    if (flagToggle2 == 0) {
-      digitalWrite(ledPinToggle2AB, LOW);
-      flagToggle2 = 0; //change flag variable; LED off
-    }
-    // if the LED is off, turn it on 
-    else if (flagToggle2 == 1){
-      digitalWrite(ledPinToggle2AB, LOW);
-      flagToggle2 = 0; //change flag variable; LED off
+    // switch toggle state 1B to low
+    if (stateToggle1B == LOW) { stateToggle1B = HIGH; }
+    else { stateToggle1B = LOW; }
     
     time = millis();    
-  }
+    }
 
-  digitalWrite(buttonPinToggle1B, stateToggle1);
-  digitalWrite(buttonPinToggle2B, stateToggle2);
+  // Toggle 1A is LOW and toggle 1B is LOW
+  else if ((readingToggle1A == LOW && previousToggle1A == HIGH) && (readingToggle1B == LOW && previousToggle1B == HIGH) && millis() - time > debounce) {
+    // switch toggle state 1A to low
+    if (stateToggle1A == LOW) { stateToggle1A = HIGH; }
+    else { stateToggle1A = LOW; }
 
-  previousToggle1 = readingToggle1;
-  previousToggle2 = readingToggle2;
+    // switch toggle state 1B to low
+    if (stateToggle1B == LOW) { stateToggle1B = HIGH; }
+    else { stateToggle1B = LOW; }
+    
+    time = millis();    
+    }
+
+
+  // TOGGLE 2
+
+  // Both toggles 2A and 2B are HIGH
+  if ((readingToggle2A == HIGH && previousToggle2A == LOW) && (readingToggle2B == HIGH && previousToggle2B == LOW) && millis() - time > debounce) {
+    // switch toggle state 2A to high
+    if (stateToggle2A == HIGH) { stateToggle2A = LOW; }
+    else { stateToggle2A = HIGH; }
+
+    // switch toggle state 2B to high
+    if (stateToggle2B == HIGH) { stateToggle2B = LOW; }
+    else { stateToggle2B = HIGH; }
+    
+    time = millis();    
+    }
+
+  // Toggle 2A is LOW (down) and toggle 2B is HIGH
+  else if ((readingToggle2A == LOW && previousToggle2A == HIGH) && (readingToggle2B == HIGH && previousToggle2B == LOW) && millis() - time > debounce) {
+    // switch toggle state 2A to low
+    if (stateToggle2A == LOW) { stateToggle2A = HIGH; }
+    else { stateToggle2A = LOW; }
+
+    // switch toggle state 2B to high
+    if (stateToggle2B == HIGH) { stateToggle2B = LOW; }
+    else { stateToggle2B = HIGH; }
+    
+    time = millis();    
+    }
+
+  // Toggle 2A is HIGH and toggle 2B is LOW
+  else if ((readingToggle2A == HIGH && previousToggle2A == LOW) && (readingToggle2B == LOW && previousToggle2B == HIGH) && millis() - time > debounce) {
+    // switch toggle state 2A to high
+    if (stateToggle2A == HIGH) { stateToggle2A = LOW; }
+    else { stateToggle2A = HIGH; }
+
+    // switch toggle state 2B to low
+    if (stateToggle2B == LOW) { stateToggle2B = HIGH; }
+    else { stateToggle2B = LOW; }
+    
+    time = millis();    
+    }
+
+  // Toggle 2A is LOW and toggle 2B is LOW
+  else if ((readingToggle2A == LOW && previousToggle2A == HIGH) && (readingToggle2B == LOW && previousToggle2B == HIGH) && millis() - time > debounce) {
+    // switch toggle state 2A to high
+    if (stateToggle2A == LOW) { stateToggle2A = HIGH; }
+    else { stateToggle2A = LOW; }
+
+    // switch toggle state 2B to low
+    if (stateToggle2B == LOW) { stateToggle2B = HIGH; }
+    else { stateToggle2B = LOW; }
+    
+    time = millis();    
+    }
+
+  previousToggle1A = readingToggle1A;
+  previousToggle1B = readingToggle1B;
+
+  previousToggle2A = readingToggle2A;
+  previousToggle2B = readingToggle2B;
 }
-
-
-
-
-void loop(){
-
-  //DRIVE
-
-  //If button the button is pressed, the state is LOW
-  if (buttonStateDrive == LOW) { 
-    // reset the other buttons to high
-    buttonStateNeutral == HIGH;
-    buttonStateReverse == HIGH;
-
-    // turn neutral LED off
-    flagNeutral == 1
-    digitalWrite(ledPinNeutral, LOW);
-    flagNeutral = 0; //change flag variable; LED off
-
-    // turn reverse LED off
-    flagReverse == 1
-    digitalWrite(ledPinReverse, LOW);
-    flagReverse = 0; //change flag variable; LED off
-
-    // if the Drive LED is on, turn it off 
-    if (flagDrive == 0) {
-      digitalWrite(ledPinDrive, HIGH);
-      flagDrive = 1; //change flag variable; LED on
-    }
-    // if the LED is off, turn it on 
-    else if (flagDrive == 1){
-      digitalWrite(ledPinDrive, LOW);
-      flagDrive = 0; //change flag variable; LED off
-    }
